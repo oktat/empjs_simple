f6fed7f4bf6bcea13012a9f79de7fbc8eca9eb4c
Sallai András <andrewteki@gmail.com>
2023-06-07 16:00:37 +0200

    Azonosítás működik

---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../database/mariadb')
+ 
+const User = sequelize.define('User', {
+    id: { 
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: { type: DataTypes.STRING,  allowNull: false  },
+    email: { type: DataTypes.STRING,  allowNull: true  },
+    password: { type: DataTypes.STRING , allowNull: false }
+})
+ 
+//A model és az adatbázis szinkronizálása, nem erőltetve.
+sequelize.sync({
+    force: false
+})
+module.exports = User
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,10 +2,15 @@ const Router = require('express');
 const router = Router();
 
 const EmployeeController = require('../controllers/employeecontroller')
+const AuthController = require('../controllers/authcontroller')
+const { verifyToken } = require('../middleware/authjwt');
 
 router.get('/employees', EmployeeController.index)
-router.post('/employees', EmployeeController.store)
+router.post('/employees', [verifyToken], EmployeeController.store)
 router.put('/employees/:id', EmployeeController.update)
 router.delete('/employees/:id', EmployeeController.destroy)
+
+router.post('/register', AuthController.register)
+router.post('/login', AuthController.login)
 
 module.exports = router
